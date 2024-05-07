ded98545a2be55064c83d5fe1598acb92d839d04
Yihui Xie <xie@yihui.name>
2024-05-07 13:31:39 -0500

    instead of disabling the *.show_progress option for tidyverse packages one by one, let's just cheat by pretending we were executing notebooks in rstudio

--- R/fuse.R
+++ R/fuse.R
@@ -343,7 +343,7 @@ fiss = function(input, output = '.R', text = NULL) {
     if (Sys.time() - t0 > td) cat(x, sep = '', file = p_out)
   }
   t0 = Sys.time(); td = getOption('litedown.progress.delay', 2)
-  opt = options(error = getOption('error'), readr.show_progress = FALSE)
+  opt = options(error = getOption('error'), rstudio.notebook.executing = TRUE)
   on.exit(options(opt), add = TRUE)
 
   # the chunk option `order` determines the execution order of chunks
