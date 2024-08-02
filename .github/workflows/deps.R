5d3f5321ee0ff8298f0b43c334d01c39f9b294c4
Yihui Xie <xie@yihui.name>
2024-08-01 19:30:21 -0500

    use wget or curl for lower versions of R to support https downloads

--- .github/workflows/deps.R
+++ .github/workflows/deps.R
@@ -1,3 +1,5 @@
-options(pkgType = "source")
+if (getRversion() <= '3.1.3') for (m in c('wget', 'curl')) if (Sys.which(m) != '') {
+  options(download.file.method = m); break
+}
 install.packages(c("commonmark", "rbibutils"), repos = "https://cloud.r-project.org")
 install.packages("xfun", repos = "https://yihui.r-universe.dev")
