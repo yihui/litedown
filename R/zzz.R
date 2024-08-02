63567990b3b509cafe49991e866e615b50d62fed
Yihui Xie <xie@yihui.name>
2024-08-02 00:48:10 -0500

    call internal functions from xfun

--- R/zzz.R
+++ R/zzz.R
@@ -10,15 +10,8 @@ has_fun = function(name, envir = baseenv()) exists(name, envir, inherits = FALSE
 if (!has_fun('isFALSE')) isFALSE = function(x) {
   is.logical(x) && length(x) == 1 && !is.na(x) && !x
 }
-if (!has_fun('startsWith')) startsWith = function(x, s) {
-  substring(x, 1, nchar(s)) == s
-}
-if (!has_fun('endsWith')) endsWith = function(x, s) {
-  n = nchar(x)
-  substring(x, n - nchar(s) + 1, n) == s
-}
+if (!has_fun('startsWith')) startsWith = function(...) xfun:::startsWith(...)
+if (!has_fun('endsWith')) endsWith = function(...) xfun:::endsWith(...)
 if (!has_fun('anyNA')) anyNA = function(x) any(is.na(x))
 if (!has_fun('dir.exists')) dir.exists = function(x) xfun::dir_exists(x)
-if (!has_fun('strrep')) strrep = function(x, times) {
-  mapply(function(x, n) paste(rep(x, n), collapse = ''), x, times, USE.NAMES = FALSE)
-}
+if (!has_fun('strrep')) strrep = function(...) xfun:::strrep(...)
