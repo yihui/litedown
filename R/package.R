31a2ee57b0f17d6189b27d0a9d6612d299c02270
Yihui Xie <xie@yihui.name>
2024-08-01 19:45:03 -0500

    conditionally use the aspell argument

--- R/package.R
+++ R/package.R
@@ -57,10 +57,10 @@ record_print.knitr_kable = function(x, ...) {
 
 # register vignette engines
 .onLoad = function(lib, pkg) {
-  tools::vignetteEngine(
-    'vignette', vig_fun(TRUE), vig_fun(FALSE), '[.]R?md$',
-    aspell = list(filter = vig_filter)
-  )
+  func = tools::vignetteEngine
+  args = list('vignette', vig_fun(TRUE), vig_fun(FALSE), '[.]R?md$')
+  if ('aspell' %in% names(formals(func))) args$aspell = list(filter = vig_filter)
+  do.call(func, args)
 }
 
 # weave or tangle?
