6756590eef5691cbdb0b42c9f68634d7de95a901
Yihui Xie <xie@yihui.name>
2024-08-02 00:39:39 -0500

    clone from github

--- .github/workflows/deps.R
+++ .github/workflows/deps.R
@@ -1,5 +1,7 @@
 if (getRversion() <= '3.1.3') for (m in c('wget', 'curl')) if (Sys.which(m) != '') {
   options(download.file.method = m); break
 }
-install.packages(c("commonmark", "rbibutils"), repos = "https://cloud.r-project.org", INSTALL_opts = "--no-help")
-install.packages("xfun", repos = "https://yihui.r-universe.dev")
+install.packages(c('commonmark', 'rbibutils'), repos = 'https://cloud.r-project.org', INSTALL_opts = '--no-help')
+system2('git', c('clone', '--depth=1', 'https://github.com/yihui/xfun'))
+system2('R', c('CMD', 'INSTALL', 'xfun'))
+unlink('xfun', recursive = TRUE)
