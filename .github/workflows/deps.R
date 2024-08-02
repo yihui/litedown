dcd2d4220d0adb1420ad345b7f4c7a356b0a9cee
Yihui Xie <xie@yihui.name>
2024-08-02 01:30:33 -0500

    yes, the minimal version we can support without much trouble is 3.2.0
    lower versions seem to have a problem with delayedAssign(), which appears difficult for me to debug, so I'll just give up here

--- .github/workflows/deps.R
+++ .github/workflows/deps.R
@@ -1,9 +1,7 @@
 if (getRversion() <= '3.2.1') for (m in c('wget', 'curl')) if (Sys.which(m) != '') {
-  options(download.file.method = m);
+  options(download.file.method = m)
   cat(sprintf('\noptions(download.file.method = "%s")\n', m), file = '~/.Rprofile', append = TRUE)
   break
 }
-install.packages(c('commonmark', 'rbibutils'), repos = 'https://cloud.r-project.org', INSTALL_opts = '--no-help')
-system2('git', c('clone', '--depth=1', 'https://github.com/yihui/xfun'))
-system2('R', c('CMD', 'INSTALL', 'xfun'))
-unlink('xfun', recursive = TRUE)
+install.packages(c('commonmark', 'rbibutils'), INSTALL_opts = '--no-help')
+install.packages('xfun', repos = 'https://yihui.r-universe.dev')
