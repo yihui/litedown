24fa61d908c0a228255ad3d93a21fbd24a7a90a4
Yihui Xie <xie@yihui.name>
2024-08-02 01:15:13 -0500

    is the lowest version of R that we could support 3.2.0?

--- .github/workflows/deps.R
+++ .github/workflows/deps.R
@@ -1,4 +1,4 @@
-if (getRversion() <= '3.1.3') for (m in c('wget', 'curl')) if (Sys.which(m) != '') {
+if (getRversion() <= '3.2.1') for (m in c('wget', 'curl')) if (Sys.which(m) != '') {
   options(download.file.method = m);
   cat(sprintf('\noptions(download.file.method = "%s")\n', m), file = '~/.Rprofile', append = TRUE)
   break
