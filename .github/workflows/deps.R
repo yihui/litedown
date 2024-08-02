a8e752b4aac8e5da2c5b164b749f16307287456f
Yihui Xie <xie@yihui.name>
2024-08-01 20:06:08 -0500

    ignore help since rbibutils uses \doi{}, which can't be recognized by lower versions of R: https://github.com/GeoBosh/rbibutils/blob/b456ae1cede569b6172174de9ddbac325c846c4e/man/rbibutils-package.Rd#L136

--- .github/workflows/deps.R
+++ .github/workflows/deps.R
@@ -1,5 +1,5 @@
 if (getRversion() <= '3.1.3') for (m in c('wget', 'curl')) if (Sys.which(m) != '') {
   options(download.file.method = m); break
 }
-install.packages(c("commonmark", "rbibutils"), repos = "https://cloud.r-project.org")
+install.packages(c("commonmark", "rbibutils"), repos = "https://cloud.r-project.org", INSTALL_opts = "--no-help")
 install.packages("xfun", repos = "https://yihui.r-universe.dev")
