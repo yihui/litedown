210fc37ab9be6532bc64594aceef70d6e4c15379
Yihui Xie <xie@yihui.name>
2024-08-02 00:54:57 -0500

    write download method to .Rprofile

--- .github/workflows/deps.R
+++ .github/workflows/deps.R
@@ -1,5 +1,7 @@
 if (getRversion() <= '3.1.3') for (m in c('wget', 'curl')) if (Sys.which(m) != '') {
-  options(download.file.method = m); break
+  options(download.file.method = m);
+  cat(sprintf('\noptions(download.file.method = "%s")\n', m), file = '~/.Rprofile', append = TRUE)
+  break
 }
 install.packages(c('commonmark', 'rbibutils'), repos = 'https://cloud.r-project.org', INSTALL_opts = '--no-help')
 system2('git', c('clone', '--depth=1', 'https://github.com/yihui/xfun'))
