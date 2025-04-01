b6d19e9c7291d9b9bc7d40c3bdf431e88cb11a8f
Yihui Xie <xie@yihui.name>
2025-04-01 11:30:07 -0500

    skip example 005 for R < 4.1.0, and use `git diff --quiet` to test differences (since `[ -z ]` doesn't work on Windows)

--- examples/_run.R
+++ examples/_run.R
@@ -7,9 +7,11 @@ for (f in litedown:::find_input('.')) {
     m = file.mtime(c(f, f2 <- xfun::with_ext(f, '.md')))
     if (!is.na(m[2]) && m[1] < m[2]) next
   }
+  # example 005 uses |>, which is available only in R >= 4.1.0
+  if (grepl('^005-', f) && getRversion() < '4.1.0') next
   xfun::Rscript_call(function(.) {
     options(width = 80, litedown.jsd_resolve = FALSE)
     if (xfun::file_ext(.) == 'md') litedown::mark(.) else litedown::fuse(., '.md')
-    if (grepl('/014-.+[.]Rmd$', .)) litedown::fiss(.)
+    if (grepl('^014-.+[.]Rmd$', .)) litedown::fiss(.)
   }, list(f))
 }
