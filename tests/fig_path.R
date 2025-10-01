63ac1b253f2b7ee682fca1dc36f7341483d1bd1f
Yihui Xie <xie@yihui.name>
2025-10-01 00:02:32 -0500

    use unlist()

--- tests/fig_path.R
+++ tests/fig_path.R
@@ -13,12 +13,12 @@ plot(1)
 
 old = litedown::reactor(fig.path = 'foo')
 out = litedown::fuse(text = src, output = 'markdown')
-stopifnot('![](<foochunk-1-1.png>' %in% strsplit(out, '\n')[[1]])
+stopifnot('![](<foochunk-1-1.png>)' %in% unlist(strsplit(out, '\n')))
 unlink('foochunk-1-1.png')
 
 litedown::reactor(fig.path = 'foo/bar')
 out = litedown::fuse(text = src, output = 'markdown')
-stopifnot('![](<foo/barchunk-1-1.png>)' %in% strsplit(out, '\n')[[1]])
+stopifnot('![](<foo/barchunk-1-1.png>)' %in% unlist(strsplit(out, '\n')))
 unlink('foo', recursive = TRUE)
 
 litedown::reactor(old)
