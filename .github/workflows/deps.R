e0b76abf51e1103512defc23fe9fda0ec96bf23b
Yihui Xie <xie@yihui.name>
2026-03-24 22:50:34 -0500

    use my own actions

--- .github/workflows/deps.R
+++
@@ -1,9 +0,0 @@
-if (getRversion() <= '3.2.1') for (m in c('wget', 'curl')) if (Sys.which(m) != '') {
-  options(download.file.method = m)
-  cat(sprintf('\noptions(download.file.method = "%s")\n', m), file = '~/.Rprofile', append = TRUE)
-  break
-}
-install.packages(
-  c('commonmark', 'xfun', 'rbibutils'), INSTALL_opts = '--no-help', quiet = TRUE,
-  repos = c('https://yihui.r-universe.dev', getOption('repos'))
-)
